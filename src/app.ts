a175cbd2161b3906c2a5224ce054cfe4f8b9b0ef
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-30 12:28:39 -0600

    feat: import routes in principal file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,16 @@
 import express from 'express';
+import swaggerUI from "swagger-ui-express";
+import specs from "./swagger/swagger";
+import chickenRoutes from "./routes/chickens.routes"
 
 const app = express();
 const PORT=3000;
+//for use jsonwebtoken
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/chickens", chickenRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
