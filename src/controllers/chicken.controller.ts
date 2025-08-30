f4bcc38dd207333c4c4ad2fe86781716e7578cf2
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-08-30 16:16:13 -0600

    feat: create update method in chicken controller

--- src/controllers/chicken.controller.ts
+++ src/controllers/chicken.controller.ts
@@ -1,3 +1,4 @@
+import { ChickenDTO } from "../dtos/chicken.dto";
 import ChickenService from "../services/chicken.service";
 import { Request, Response } from "express";
 
@@ -17,11 +18,11 @@ export default class ChickenController {
     }
   };
 
-  getChickenById=async(req:Request, res:Response)=>{
+  getChickenById = async (req: Request, res: Response) => {
     const chickenId: string | undefined = req.params.id;
 
     if (!chickenId) {
-        return res.status(400).json({ message: "Chicken ID is required." });
+      return res.status(400).json({ message: "Chicken ID is required." });
     }
 
     try {
@@ -32,8 +33,64 @@ export default class ChickenController {
       } else {
         res.status(404).json({ message: "Chicken not found" });
       }
-    } catch{
+    } catch {
       res.status(500).json({ message: "Failed to get chicken" });
+    }
+  };
+
+  addChicken = async (req: Request, res: Response) => {
+    const {
+      loteId,
+      race,
+      birthdate,
+      currentWeight,
+      healthStatus,
+      dateReadyForMeat,
+      diseaseHistory,
+      name,
+    } = req.body;
+
+    const newChickenData: ChickenDTO = {
+      loteId,
+      race,
+      birthdate,
+      currentWeight,
+      healthStatus,
+      dateReadyForMeat,
+      diseaseHistory,
+      name,
+    };
+
+    try {
+      const response= await this.service.addChicken(newChickenData);
+      res.status(201).json({
+        message: "Chicken added correctly",
+        status: response,
+      });
+    } catch {
+      res.status(400).json({ message: "Failed to add the chicken" });
+    }
+  };
+
+  updateChicken=async(req:Request, res:Response)=>{
+    const id:string | undefined = req.params.id;
+    const data = req.body;
+
+     if (!id) {
+      return res.status(400).json({ message: "Chicken ID is required." });
+    }
+
+
+    try {
+      const success = await this.service.updateChicken(id, data);
+
+      if (success) {
+        res.status(200).json({ message: "Chicken updated successfully" });
+      } else {
+        res.status(404).json({ message: "Chicken not found" });
+      }
+    } catch {
+      res.status(400).json({ message: "Failed to update chicken" });
     }
   }
 }
