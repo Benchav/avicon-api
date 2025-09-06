07f436d20e779ef53b741be8c6548e4de7c52117
benchav <joshua44benja@gmail.com>
2025-09-05 18:21:33 -0600

    lote.data

---
+++ src/data/lotes.data.ts
@@ -0,0 +1,23 @@
+import LoteModel, { LoteStatus } from "../models/lote.model";
+
+export const lotesData: LoteModel[] = [
+  new LoteModel(
+    "L1",
+    "A1",
+    "Lote Norte",
+    "Managua",
+    120,
+    new Date("2023-01-01"),
+    LoteStatus.ACTIVO,
+    "Lote principal de producción"
+  ),
+  new LoteModel(
+    "L2",
+    "B1",
+    "Lote Sur",
+    "Masaya",
+    80,
+    new Date("2023-02-15"),
+    LoteStatus.ACTIVO
+  ),
+];
