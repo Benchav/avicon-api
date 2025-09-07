029176666c86f58ad7b1161c1dfbd2618989fe90
benchav <joshua44benja@gmail.com>
2025-09-06 18:30:03 -0600

    agree alert route app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,18 @@ import specs from "./swagger/swagger";
 import chickenRoutes from "./routes/chickens.routes"
 import lotesRoutes from "./routes/lotes.routes";
 import saludRoutes from "./routes/salud.routes";
+import alertsRoutes from "./routes/alerts.routes";
 
 const app = express();
 const PORT=3000;
-//for use jsonwebtoken
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 app.use("/chickens", chickenRoutes);
 app.use("/lotes", lotesRoutes);
-app.use("/salud", saludRoutes)
+app.use("/salud", saludRoutes);
+app.use("/alerts", alertsRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
