a50121d9118d6c6e48b6040bbdcd6f54859df8c6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-13 11:07:08 -0600

    feat: create enums with the basic operations in sql like read and write

---
+++ src/builders/sqlOperations.enum.ts
@@ -0,0 +1,10 @@
+export enum SqlWriteOperation {
+  Create='CREATE',
+  Update='UPDATE',
+  Delete='DELETE',
+}
+
+export enum SqlReadOperation {
+  Select='SELECT',
+  SelectById='SELECT_BY_ID',
+}
