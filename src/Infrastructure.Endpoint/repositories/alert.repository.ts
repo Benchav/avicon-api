b5cbf651aef75a8b9c52b99472009f4048c8df56
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-17 19:23:05 -0600

    feat: add method get by id in alert repository class

--- src/Infrastructure.Endpoint/repositories/alert.repository.ts
+++ src/Infrastructure.Endpoint/repositories/alert.repository.ts
@@ -6,38 +6,62 @@ import { ISqlCommandOperationBuilder } from "../interfaces/sqlCommandOperation.i
 import { EntityType } from "../utils/entityTypes";
 
 export class AlertRepository implements IAlertRepository {
-    private readonly _operationBuilder: ISqlCommandOperationBuilder;
-    private readonly _connection: ISingletonSqlConnection;
-    
-    constructor(operationBuilder: ISqlCommandOperationBuilder, connection: ISingletonSqlConnection) {
-        this._operationBuilder = operationBuilder;
-        this._connection = connection;
-    }
+  private readonly _operationBuilder: ISqlCommandOperationBuilder;
+  private readonly _connection: ISingletonSqlConnection;
 
-    async getAll(): Promise<AlertModel[]> {
-        const readCommand = this._operationBuilder.Initialize(EntityType.Alert).WithOperation(SqlReadOperation.Select).BuildReader();
-        const rows = await this._connection.executeQuery(readCommand);
-        return rows.map(row => new AlertModel(
-            row["ID"],
-            row["TITLE"],
-            row["DESCRIPTION"],
-            row["LEVEL"],
-            row["IS_RESOLVED"],
-            row["CREATED_AT"],
-            row["LOTE_ID"]
-        ));
-    }
-    getById(id: string): Promise<AlertModel | null> {
-        throw new Error("Method not implemented.");
-    }
-    create(alert: AlertModel): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(alert: AlertModel): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(alert: AlertModel): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+  constructor(
+    operationBuilder: ISqlCommandOperationBuilder,
+    connection: ISingletonSqlConnection
+  ) {
+    this._operationBuilder = operationBuilder;
+    this._connection = connection;
+  }
 
+  async getAll(): Promise<AlertModel[]> {
+    const readCommand = this._operationBuilder
+      .Initialize(EntityType.Alert)
+      .WithOperation(SqlReadOperation.Select)
+      .BuildReader();
+    const rows = await this._connection.executeQuery(readCommand);
+    return rows.map(
+      (row) =>
+        new AlertModel(
+          row["ID"],
+          row["TITLE"],
+          row["DESCRIPTION"],
+          row["LEVEL"],
+          row["IS_RESOLVED"],
+          row["CREATED_AT"],
+          row["LOTE_ID"]
+        )
+    );
+  }
+  async getById(id: string): Promise<AlertModel | null> {
+    const readCommand = this._operationBuilder
+      .Initialize(EntityType.Chicken)
+      .WithOperation(SqlReadOperation.SelectById)
+      .WithId(id)
+      .BuildReader();
+    const row = await this._connection.executeScalar(readCommand);
+    if (!row) return null;
+    return new AlertModel(
+      row["ID"],
+      row["TITLE"],
+      row["DESCRIPTION"],
+      row["LEVEL"],
+      row["IS_RESOLVED"],
+      row["CREATED_AT"],
+      row["LOTE_ID"]
+    );
+  }
+  
+  create(alert: AlertModel): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  update(alert: AlertModel): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  delete(alert: AlertModel): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
 }
