cddc0de8873ae55791abe32f4b14d87a7d618ef9
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-18 18:46:42 -0600

    refactor: dynamic port to listen

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import alertsRoutes from "./WebApi/routes/alerts.routes";
 
 
 const app = express();
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: false }));
