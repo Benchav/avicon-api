1b987db0b6b68ed73c516d526df55aa9a7fb8de0
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-09-20 05:33:14 -0600

    feat: register classes in container for dependency injections

--- src/WebApi/container.ts
+++ src/WebApi/container.ts
@@ -11,11 +11,21 @@ import { ChickenRepository } from '../Infrastructure.Endpoint/repositories/chick
 import { IChickenService } from '../Domain.Endpoint/interfaces/services/chickenService.interface';
 import ChickenService from '../Domain.Endpoint/services/chicken.service';
 import ChickenController from './controllers/chicken.controller';
+import { IAlertRepository } from '../Domain.Endpoint/interfaces/repositories/alertRepository.interface';
+import { AlertRepository } from '../Infrastructure.Endpoint/repositories/alert.repository';
+import { IAlertService } from '../Domain.Endpoint/interfaces/services/alertService.interface';
+import AlertService from '../Domain.Endpoint/services/alert.service';
+import AlertController from './controllers/alert.controller';
 
 // Registrar clases concretas
 container.registerSingleton<ISingletonSqlConnection>('ISingletonSqlConnection', SingletonSqlConnection);
 container.registerSingleton<IEntitiesService>('IEntityService', EntitiesService);
 container.register<ISqlCommandOperationBuilder>('IOperationBuilder', { useClass: SqlCommandOperationBuilder });
+//chicken
 container.register<IChickenRepository>('IChickenRepository', { useClass: ChickenRepository });
 container.register<IChickenService>('IChickenService', { useClass: ChickenService });
 container.register<ChickenController>('ChickenController', { useClass: ChickenController });
+//alert
+container.register<IAlertRepository>('IAlertRepository', { useClass: AlertRepository });
+container.register<IAlertService>('IAlertService', { useClass: AlertService });
+container.register<AlertController>('AlertController', { useClass: AlertController });
